392d642accee9c1dc34fbae9d4d8b113b0dd4edb
EC2 Default User <ec2-user@ip-172-31-1-161.ap-northeast-1.compute.internal>
2023-01-30 15:24:39 +0000

    test 1

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,6 @@ class Book < ApplicationRecord
   has_one_attached :profile_image
   
   validates :title, presence: true
-  validates :body, presence: true
+  validates :body, length: {maximum: 200}, presence: true
  
 end
